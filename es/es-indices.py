98519be6197fe43267c4bb2cdbfff3ccb39c1295
Patrycja <patrycja.p9428@gmail.com>
2021-01-31 18:01:24 +0100

    Refactor check_json and get_disk_usage functions

--- es/es-indices.py
+++ es/es-indices.py
@@ -4,6 +4,7 @@ import json
 
 url_blocked = 'http://localhost:9200/_all/_settings'
 url_disk = 'http://localhost:9200/_cat/allocation?h=disk.percent'
+url_indices = 'http://localhost:9200/_cat/indices?h=creation.date.string&s=creation.date'
 header = {
     'Content-Type': 'application/json'
 }
@@ -17,24 +18,26 @@ get_blocked = requests.get(url_blocked).json()
 def check_json(json_arg):
     data = json.dumps(json_arg)
     if "read_only_allow_delete" in data:
-        return "blocked"
+        requests.put(url_blocked, data=indices_unblocked, headers=header)
+        print('Indexes unblocked, put "index.blocks.read_only_allow_delete": "null"')
     else:
-        return "not blocked"
+        print('OK.Indexes not blocked')
 
 
 def get_disk_usage(json_arg):
     data = json.dumps(json_arg)
     if data >= '85':
+        requests.delete()
         return "full"
     else:
         return "free"
 
 
+check_json(get_blocked)
 # Unlock indices
-if check_json(get_blocked) == "blocked":
-    requests.put(url_blocked, data=indices_unblocked, headers=header)
-    print('Indexes unblocked, put "index.blocks.read_only_allow_delete": "null"')
-else:
-    print('OK')
-    exit(0)
-
+# if check_json(get_blocked) == "blocked":
+#     requests.put(url_blocked, data=indices_unblocked, headers=header)
+#     print('Indexes unblocked, put "index.blocks.read_only_allow_delete": "null"')
+# else:
+#     print('OK')
+#     exit(0)
