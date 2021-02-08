c6ffd49f603e879adfe9d10717cc7f4a573ba835
Patrycja <patrycja.p9428@gmail.com>
2021-02-08 20:42:28 +0100

    Refactor es-indices.py, import Elastisearch library

--- es/es-indices.py
+++ es/es-indices.py
@@ -1,43 +1,13 @@
 #!/usr/bin/env python3
 import requests
 import json
+from elasticsearch import Elasticsearch
+# connect to the es cluster
+es = Elasticsearch("localhost:9200")
+print(es.cluster.state()['cluster_name'])
+# print(es.cluster.state(metric='_all'))
+# create an index
+# es.indices.create('index-001')
 
-url_blocked = 'http://localhost:9200/_all/_settings'
-url_disk = 'http://localhost:9200/_cat/allocation?h=disk.percent'
-url_indices = 'http://localhost:9200/_cat/indices?h=creation.date.string&s=creation.date'
-header = {
-    'Content-Type': 'application/json'
-}
-indices_unblocked = '{"index.blocks.read_only_allow_delete": null}'
-
-disk_usage = int((requests.get(url_disk)).text)
-get_blocked = requests.get(url_blocked).json()
-
-
-# Check if http://localhost:9200/_all/_settings contains "read_only_allow_delete"
-def check_json(json_arg):
-    data = json.dumps(json_arg)
-    if "read_only_allow_delete" in data:
-        requests.put(url_blocked, data=indices_unblocked, headers=header)
-        print('Indexes unblocked, put "index.blocks.read_only_allow_delete": "null"')
-    else:
-        print('OK.Indexes not blocked')
-
-
-def get_disk_usage(json_arg):
-    data = json.dumps(json_arg)
-    if data >= '85':
-        requests.delete()
-        return "full"
-    else:
-        return "free"
-
-
-check_json(get_blocked)
-# Unlock indices
-# if check_json(get_blocked) == "blocked":
-#     requests.put(url_blocked, data=indices_unblocked, headers=header)
-#     print('Indexes unblocked, put "index.blocks.read_only_allow_delete": "null"')
-# else:
-#     print('OK')
-#     exit(0)
+print(es.cat.indices(h='health,uuid'))
+print(es.cat.allocation(h='disk.percent'))
